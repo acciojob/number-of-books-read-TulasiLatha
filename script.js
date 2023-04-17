cedc8732bc4c9f965e231bb146ea56f211f3e72f
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-17 16:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count=0;
+	if(readingStatus== true){
+		count++;
+	}
+	return count;
 };
 
 // Do not change the code below
